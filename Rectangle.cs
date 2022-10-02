df73709eda930976b6feb5cb1fc1e07c84d4fe99
Andriy Rachenko <108069918+Andrew1929@users.noreply.github.com>
2022-10-02 20:47:08 +0300

    Add files via upload

--- Rectangle.cs
+++ Rectangle.cs
@@ -8,26 +8,29 @@ namespace OOP_LB_3._1
 {
     class Rectangle
     {
-        public double side1 { get; }
-        public double side2 { get; }
+        public double Area { get; }
+        public double Perimeter { get; }
+        private double side1;
+        private double side2;
         public Rectangle (double side1, double side2)
         {
+            
             this.side1 = side1;
             this.side2 = side2;
         }
         public double AreaCalculator()
         {
-            double area = side1 * side2;
+           double Area = side1 * side2;
             Console.WriteLine($"First side: {side1}, Second side: {side2}\n" +
-                $"Area = first side * second side : {side1} * {side2} = {area}");
-            return area;
+                $"Area = first side * second side : {side1} * {side2} = {Area}");
+            return Area;
         }
         public double PerimeterCalculator()
         {
-            double perimeter = side1 * 2 + side2 * 2;
+            double Perimeter = side1 * 2 + side2 * 2;
             Console.WriteLine($"First side: {side1}, Second side: {side2}\n" +
-               $"Perimetr = first side * 2 + second side * 2 : {side1} * 2 + {side2} * 2 = {perimeter}");
-            return perimeter;
+               $"Perimetr = first side * 2 + second side * 2 : {side1} * 2 + {side2} * 2 = {Perimeter}");
+            return Perimeter;
         }
     }
 }
